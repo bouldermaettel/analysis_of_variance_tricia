c86031f3b8260448c19b77e78060ead8bba666f8
bouldermaettel <bouldermaettel@gmail.com>
2023-05-21 22:15:35 +0200

    adapted to tricia specific question

--- main.R
+++ main.R
@@ -14,8 +14,15 @@
 remove(list=ls())
 
 library(readxl)
+library(dplyr)
+library(tidyverse)
 
+numeric_cols <- c("S", "D", "P")
+col_types <- rep("default", ncol(readxl::read_excel("example.xlsx", sheet = "Sheet1")))
+col_types[names(col_types) %in% numeric_cols] <- "numeric"
 data_tot <- read_excel("Varianzanalyse_v3_RESULTS.xlsx", 1)
+
+data_tot <- data_tot %>% mutate(k = ifelse(is.na(as.numeric(k)), NA, as.numeric(k)))
 head(data_tot)
 str(data_tot)
 
@@ -25,11 +32,11 @@ data_tot$Testperson <- factor(data_tot$Testperson)
 
 
 # define alpha's for prediction and for the confidence interval of the mean difference
-alpha.pred <- 0.05 
-alpha.conf  <- 0.1
+alpha_pred <- 0.05 
+alpha_conf  <- 0.1
 
 # number of future n number of within series differencies
-n.test <- 5
+n_test <- 5
 
 tasks = c("S", "D", "P")
 
@@ -37,101 +44,100 @@ tasks = c("S", "D", "P")
 
 ## To test the script
 # i <- 63793
-# k <- s
+# k <- "S"
 
-# Loop over VK_IDs to get cases
-for (i in na.omit(unique(data_tot$VK_ID))) {
+for (k in unique(tasks)) {
+  
+  diff_vector <- c()
 
-  case <- na.omit(data_tot[data_tot$VK_ID == i,])
+  # Loop over VK_IDs to get cases
+  for (i in na.omit(unique(data_tot$VK_ID))) {
   
-  # Loop over tasks (severity, detectability)
-  
-  for (k in unique(tasks)) {
+    id <- na.omit(data_tot[data_tot$VK_ID == i, ])
     
-    case_task <- as.numeric(case[[k]])
-    
+    case <- id %>% pull(k) %>% as.numeric()
+
     # Get all combinations of differences
-    all_combs <- combn(case_task, 2, function(x) diff(x)) 
+    diff <- combn(case, 2, function(x) diff(x)) 
     
-    # add all combs with different sign
-    # all_combs = c(all_combs, -1*(all_combs))
-    
-    # calculate within differencies
-    diff <- na.omit(data.sample$result[data$replicate==1] - data.sample$result[data$replicate==2])
-    sd(diff)
+    diff_vector <- c(diff_vector, diff)
+
+  }
     
     #####################################################################
     # using bootstrap to define standard deviation of mean differences  #
     #####################################################################
     
     # Only the sign is random (for the bootstrap), so work with absolute values and randomly allocate a plus or a minus
-    abs.diff <- abs(diff)
+    abs_diff <- abs(diff_vector)
   
     # qqnorm(diff)
-    # qqnorm(abs.diff)
-    # qqline(abs.diff)
+    # qqnorm(abs_diff)
+    # qqline(abs_diff)
     
-    mu.abs <- mean(abs.diff)
-    sd.abs <- sd(abs.diff)
+    mu_abs <- mean(abs_diff)
+    sd_abs <- sd(abs_diff)
     
     # calculate mean and CI for abs diff (should be within the AC!)
-    abs.diff.CI <- mu.abs + qt(c(0.05,0.95), length(abs.diff)-1)*sd.abs
+    abs_diff_CI <- mu_abs + qt(c(0.05,0.95), length(abs_diff)-1)*sd_abs
     
     # define empty vectors
-    sd.diff <- NA
-    mean.diff <- NA
-    diff.numb <- NA
+    sd_diff <- NA
+    mean_diff <- NA
+    diff_numb <- NA
     set.seed(123)
     for (z in 1:1e4){
-      sign <- rbinom(length(abs.diff),1,0.5)*2-1 # binomial distribution with 50% 0 and 50% 1: factor 2 and plus 1 to transform to -1 and 1
-      sign.diff <-sign*abs.diff
-      sd.diff[z] <- sd(sign.diff)
-      mean.diff[z] <- mean(sign.diff)
+      sign <- rbinom(length(abs_diff),1,0.5)*2-1 # binomial distribution with 50% 0 and 50% 1: factor 2 and plus 1 to transform to -1 and 1
+      sign_diff <-sign*abs_diff
+      sd_diff[z] <- sd(sign_diff)
+      mean_diff[z] <- mean(sign_diff)
        
       if (z == 1) {
         
-        diff.numb <- sign.diff
+        diff_numb <- sign_diff
       } else {
-        diff.numb <- append(diff.numb, sign.diff)
+        diff_numb <- append(diff_numb, sign_diff)
       }
     }
     
 
-    # qqnorm(diff.numb)
-    # qqline(diff.numb)
+    # qqnorm(diff_numb)
+    # qqline(diff_numb)
     # 
-    # qqnorm(mean.diff)
-    # qqline(mean.diff)
+    # qqnorm(mean_diff)
+    # qqline(mean_diff)
     
-    sd(diff.numb)
+    sd(diff_numb)
 
-    # calculate sd based on mean.diff and multipy with sqrt(n)
-    sd.diff.boot <- sd(mean.diff)*sqrt(length(abs.diff))
+    # calculate sd based on mean_diff and multipy with sqrt(n)
+    sd_diff_boot <- sd(mean_diff)*sqrt(length(abs_diff))
     
-    sd.diff.boot2 <- mean(sd.diff)
-    # qqnorm(sd.diff)
-    sd.diff.boot
-    mean.diff.boot <- mean(mean.diff)
-    # qqnorm(mean.diff)
-    sd(mean.diff)
-    mean.diff.boot # must be close to zero!
+    sd_diff_boot2 <- mean(sd_diff)
+    # qqnorm(sd_diff)
+    sd_diff_boot
+    mean_diff_boot <- mean(mean_diff)
+    # qqnorm(mean_diff)
+    sd(mean_diff)
+    mean_diff_boot # must be close to zero!
     
     
     #######################################################################################
-    # The within standard deviation approach! do not forget to define series as factor!   #
+    # The within standard deviation approach! do not forget to define Testperson as factor!   #
     #######################################################################################
+    data_tot <- data_tot %>% mutate(k = ifelse(is.na(as.numeric(k)), NA, k))
+    data_tot[k] = as.numeric(data_tot[k])
     
-    summary(fit <- lm (result ~  as.factor(serie), data = data.sample))
+    summary(fit <- lm (as.numeric(k) ~  as.factor(Testperson), data = data_tot))
     anova(fit)
    
-    sd.within <- sqrt(anova(fit)["Residuals", "Mean Sq"])
-    sd.within*sqrt(2) # is close to sd
+    sd_within <- sqrt(anova(fit)["Residuals", "Mean Sq"])
+    sd_within*sqrt(2) # is close to sd
     
     # simulate data sets to calculate differencies
     set.seed(123)
-    diffs <- rnorm(1e5,0,sd.within)-rnorm(1e5,0,sd.within)
-    mean.diff <- mean(diffs)
-    sd <- sd(diffs)  # is equivalent to the sd.within*sqrt(2)
+    diffs <- rnorm(1e5,0,sd_within)-rnorm(1e5,0,sd_within)
+    mean_diff <- mean(diffs)
+    sd <- sd(diffs)  # is equivalent to the sd_within*sqrt(2)
     
     qqnorm(diffs)
     qqline(diffs)
@@ -147,14 +153,14 @@ for (i in na.omit(unique(data_tot$VK_ID))) {
     
     combs <- list(comb4,comb5, comb6)
     df <- data.frame(NA,NA,NA, NA, NA, NA)
-    df <- df[,1:length(abs.diff)]
+    df <- df[,1:length(abs_diff)]
     CIs <- data.frame(NA,NA) # used for consistency check!
     CIs2 <- data.frame(NA,NA) # used for consistency check!
     
-    for (l in 1: nrow(combs[[length(abs.diff)-3]])) {
-      df[l,] <-  abs.diff*combs[[length(abs.diff)-3]][l,]
-      CIs[l,] <- mean(as.numeric(df[l,])) + qt(c(alpha.conf/2,1-alpha.conf/2),length(abs.diff)-1)*sd(df[l,])/sqrt(length(abs.diff))
-      CIs2[l,] <- c(t.test(as.numeric(df[l,]), conf.level = 0.9)$conf.int[1],t.test(as.numeric(df[l,]))$conf.int[2])
+    for (l in 1: nrow(combs[[length(abs_diff)-3]])) {
+      df[l,] <-  abs_diff*combs[[length(abs_diff)-3]][l,]
+      CIs[l,] <- mean(as.numeric(df[l,])) + qt(c(alpha_conf/2,1-alpha_conf/2),length(abs_diff)-1)*sd(df[l,])/sqrt(length(abs_diff))
+      CIs2[l,] <- c(t.test(as.numeric(df[l,]), conf_level = 0.9)$conf.int[1],t.test(as.numeric(df[l,]))$conf.int[2])
 
     }
       
@@ -163,8 +169,8 @@ for (i in na.omit(unique(data_tot$VK_ID))) {
     
     # calculate means, sd of means and sd of values
       means <- apply(df,1,mean )
-      sd.means <- sd(means)
-      sd.permut <- sd.means*sqrt(length(abs.diff))
+      sd_means <- sd(means)
+      sd_permut <- sd_means*sqrt(length(abs_diff))
       
       qqnorm(means)
       qqline(means)
@@ -173,31 +179,31 @@ for (i in na.omit(unique(data_tot$VK_ID))) {
       CImin <- min(CIs[,2])*2
       CImax <- max(CIs[,2])*2
       
-      CIlower.avg <- mean(CIs[,1])
-      CIupper.avg <- mean(CIs[,2])
+      CIlower_avg <- mean(CIs[,1])
+      CIupper_avg <- mean(CIs[,2])
       
-      CIlower2.avg <- mean(CIs2[,1])
-      CIupper2.avg <- mean(CIs2[,2])
+      CIlower2_avg <- mean(CIs2[,1])
+      CIupper2_avg <- mean(CIs2[,2])
       
-      # calculate (1-alpha.conf)-confidence interval for the mean difference of the future measures (n.test)
-      CI <- qt(c(alpha.conf/2,1-alpha.conf/2),n.test-1)*sd/sqrt(n.test)
+      # calculate (1-alpha_conf)-confidence interval for the mean difference of the future measures (n.test)
+      CI <- qt(c(alpha_conf/2,1-alpha_conf/2),n_test-1)*sd/sqrt(n_test)
       CIupper <- CI[2]
       CIlower <- CI[1]
       
-      # calculate (1-alpha.pred)-predicion intervals from original samples
-      pred <-  sqrt(1/length(abs.diff) + 1/n.test)*sd*qt(c(alpha.pred/2,1-alpha.pred/2),length(abs.diff)-1)
+      # calculate (1-alpha_pred)-predicion intervals from original samples
+      pred <-  sqrt(1/length(abs_diff) + 1/n_test)*sd*qt(c(alpha_pred/2,1-alpha_pred/2),length(abs_diff)-1)
       
-      # # calculation acc. to pharmacopoe and according to A149 (not PI for mean but for single value)
-      # pred.pharm <-  sqrt(1/length(abs.diff) + 1)*sd.diff.boot*qt(c(0.05,0.95),length(abs.diff)-1) # is used as AC, so compare it to the AC
+      # # calculation acc_ to pharmacopoe and according to A149 (not PI for mean but for single value)
+      # pred_pharm <-  sqrt(1/length(abs_diff) + 1)*sd_diff_boot*qt(c(0.05,0.95),length(abs_diff)-1) # is used as AC, so compare it to the AC
       
-      # calculate AC (PI + each side 1/2 (1-alpha.conf)-%-CI length)
+      # calculate AC (PI + each side 1/2 (1-alpha_conf)-%-CI length)
       AC   <- pred + CI
       ACupper <- AC[2]
       AClower <- AC[1]
       AC
       
     
-    setwd("\\\\euchbrnfil01\\Matthias.Mueller$\\MyDocs\\R Scripts\\Brigitte Equivalenztest Differenzen/WithinSeries/plots")
+    setwd("/plots")
     
     png(filename=paste("Equivalence Limits_", i, k, ".png" ,sep = ""), 
         type="cairo",
@@ -229,7 +235,7 @@ for (i in na.omit(unique(data_tot$VK_ID))) {
     text (x = 5, y = AC[2]-a, labels = c("Upper acceptance limit"), col = "red")
     text (x = 5, y = AC[1]+a, labels = c("Lower acceptance limit"), col = "red")
     
-    legend("topright", legend =c(paste("sd bootstrap approach     =" , round(sd.diff.boot, 3)), 
+    legend("topright", legend =c(paste("sd bootstrap approach     =" , round(sd_diff.boot, 3)), 
                                  paste("sd within approach            =", round(sd, 3)),
                                  paste("sd permutation approach =", round(sd.permut,3))))
     
